9ddb00d3ef6201eacc4e459a318f5d81fa12061f
Pema Dorji Sherpa <pemarekdendorjee@gmail.com>
2021-11-09 21:56:35 +0600

    Update _dbconnect.php

--- php/partials/_dbconnect.php
+++ php/partials/_dbconnect.php
@@ -1,12 +1,23 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "qbonanza";
-$conn = mysqli_connect($host,$user,$pass,$db);
+class Database{
+ 
+    private $database = "qbonanza"; //database name
+    private $uid = "root"; //username
+    private $pwd = ""; //password
+    public $conn;
 
-if(!$conn){
-    echo "ERROR LMAO";
+ // get the database connection
+ public function getConnection(){
+
+     $this->conn = null;
+
+     try{
+         $this->conn = new PDO("mysql:host=localhost;dbname=" . $this->database, $this->uid, $this->pwd);
+         $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+     }catch(PDOException $exception){
+         echo "Connection error : " . $exception->getMessage();
+     }
+
+     return $this->conn;
+ }
 }
-
-?>
