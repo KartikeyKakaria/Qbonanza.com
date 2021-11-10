590ac8f4fdc9ffce299e0b692dfa43510e623d1a
KartikeyKakaria <kartikeykakaria2@gmail.com>
2021-11-10 19:15:39 +0530

    did some corrections that happened during pull request

--- js/signup.js
+++ js/signup.js
@@ -3,40 +3,37 @@ $(document).ready(function() {
     $("#signup").click(function() {
         console.log(name);
     })
-    $("#signup").click(function(){
+    $("#signup").click(function() {
         //   e.preventDefault();
         //   ajax query                
         let Password = $("#Passowrd").value;
         let confPassword = $("#confPassword").value;
-        if(Password == confPassword){
+        if (Password == confPassword) {
             let name = $("#name").value;
             let email = $("#email").value;
-            let Age = $("#Age").value;  
-            $.post("/Qbonanza.com/php/signup.php",{
+            let Age = $("#Age").value;
+            $.post("/Qbonanza.com/php/signup.php", {
                 // these are the variables sent via ajax to the php
                 nam: name,
-                emaile:email,
-                ager:Age,
-                passwordo:Password
-        },function(data, status){
-            if(status=="success"){
-                if(data=="success"){
-                    console.log("Successfully signed up!");
+                emaile: email,
+                ager: Age,
+                passwordo: Password
+            }, function(data, status) {
+                if (status == "success") {
+                    if (data == "success") {
+                        console.log("Successfully signed up!");
+                    } else {
+                        console.log(data);
+                    }
+                    console.log(data);
+                } else {
+                    console.log("Error 404")
                 }
-                else{
-                   console.log(data);
-                }
-                console.log(data);
-            }
-            else{
-                console.log("Error 404")
-            }
-        })
-      }
-      else{
-          console.log("Passwords dont match");
-          console.log(Password)
-          console.log(confPassword)
-      }
-  })
+            })
+        } else {
+            console.log("Passwords dont match");
+            console.log(Password)
+            console.log(confPassword)
+        }
+    })
 })
--- php/signup.php
+++ php/signup.php
@@ -1,24 +1,36 @@
 <?php
-$(document).ready(function() {
-    $("#signup").submit(function(event){
-        //event.preventDefault(); //prevent default action 
-        var post_url = $(this).attr("action"); //get form action url
-        var request_method = $(this).attr("method"); //get form GET/POST method
-        var form_data = $(this).serialize(); //Encode form elements for submission
-        
-        let name = $("#name").value;
-        let email = $("#email").value;
-        let age = $("#age").value;
-        let password = $("#password").value;
-        $.post("post_url",{
-            name : name,
-            email : email,
-            age : age,
-            password : password
-        },function(response){ 
-            //alert("success");
-            console.log(response.msg);
-        })
-       
-    });
-})
+include 'partials/_dbconnect.php';
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];  
+        $age = $_POST['age']; 
+        $email = $_POST['email'];;
+        $password = $_POST['password']; 
+        $sql = "INSERT INTO `user` (`id`, `name`, `email`, `age`, `password`, `date`) VALUES ('', '$name', '$email', '$age', '$password', current_timestamp())";
+        try{
+            //Get DB Object
+            $database = new Database();
+            //Connect
+            $db = $database->getConnection();
+            //prepare sql1 statement
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $id = $db->lastInsertId();
+            if($id){
+                $message = 'Signup successfully';
+                json_encode([
+                    'msg' => $message,
+                ]);
+            }else{
+                $message = 'Something went wrong';
+                echo json_encode([
+                    'msg' => $message,
+                ]);
+            }
+        }catch(PDOException $e){
+            echo '"error": {"text": '.$e->getMessage().'}';
+        }
+    }else{
+        echo 'method should be post';
+    }
+
+?>
