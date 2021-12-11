1093e7c4ec42b1b1150c2acdccb13b391c38da74
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2021-12-11 20:51:39 +0530

    hosting with heroku will be don later :) lets stick to localhost for now

--- php/partials/_dbconnect.php
+++ php/partials/_dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 class Database{
  
-    private $database = "ddk2dl27bum747"; //database name
-    private $uid = "ristyykeyooujp"; //username
-    private $pwd = "7a9de02f83b55f8326aeb12da89cf44a64ee89b8e6fa7b9e5b85f65646c3137e"; //password
+    private $database = "qbonanza"; //database name
+    private $uid = "root"; //username
+    private $pwd = ""; //password
     public $conn;
 
  // get the database connection
@@ -12,7 +12,7 @@ class Database{
      $this->conn = null;
 
      try{
-         $this->conn = new PDO("mysql:host=ec2-50-16-241-192.compute-1.amazonaws.com;dbname=" . $this->database, $this->uid, $this->pwd);
+         $this->conn = new PDO("mysql:localhost;dbname=" . $this->database, $this->uid, $this->pwd);
          $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
      }catch(PDOException $exception){
          echo "Connection error : " . $exception->getMessage();
