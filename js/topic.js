519cb2c9c74d4ecd4e3464680b18d248db9be9ed
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2021-12-25 20:05:49 +0530

    did the js work

--- js/topic.js
+++ js/topic.js
@@ -2,65 +2,67 @@ window.onload = ()=>{
     const user = localStorage.getItem('user');
     const href = window.location.href;
     const LastChar = href[href.length - 1];
-    console.log(LastChar);
+    // console.log(LastChar);
+    function showQuestions(id){
+        let dat = {
+            id:id,
+        }
+        
+        let data1 = JSON.stringify(dat);
+        let params = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: data1,
+        }
+        fetch("/Qbonanza.com/php/topic.php", params)
+        .then(response => response.json())
+        .then(data => {
+            
+            let questions = data.question;
+            let str ="";
+            questions.forEach(question => {
+                // let name = ""; , 
+                str+="<div class='question'><h4>"+question.title+"</h4><p>"+question.description+"</p><br><p>Posted by "+question.id+" on "+question.date+"</p></div>";
+                document.querySelector("#questions").innerHTML = str;
+                // .catch(err => console.error(err))
+                
+            })
+        })
+    }
+    showQuestions(LastChar);
     if(user == null){
         document.querySelector("#ask").innerHTML = "<h2>Please login to ask questions</h2>"
     }
     else{
-        document.querySelector("#ask").innerHTML = '<label for="title">Title:</label><input type="text" class="form-control" placeholder="enter title" ="title"><br><label for="description">Description:</label><textarea placeholder="enter title" id="description" cols="15" rows="10" class="form-control"></textarea>';
-    }
-    let dat = {
-        id:LastChar,
-    }
-    // if(user == null) {
-    //     let dat = { 
-    //         post:false,
-    //         id:LastChar,
-    //     };
-    // }
-    // else{
-    //     let dat = {
-    //         post:true,
-    //         id:LastChar,
-    //         title:document.querySelector('#title').value,
-    //         content:document.querySelector('#content').value,
-    //     }
-    // }
-    let data1 = JSON.stringify(dat);
-    let params = {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: data1,
-    }
-    fetch("/Qbonanza.com/php/topic.php", params)
-    .then(response => response.json())
-    .then(data => {
-        
-        let questions = data.question;
-        let str ="";
-        questions.forEach(question => {
-            let name = "";
-            dat = {
-                method:'post',
-                hedares:{
-                    'Content-type': 'application/json',
-                },
-                body:JSON.stringify({id:question.user_id})
+        document.querySelector("#ask").innerHTML = '<label for="title">Title:</label><input type="text" class="form-control" placeholder="enter title" id="title"><br><label for="description">Description:</label><textarea placeholder="enter title" id="description" cols="15" rows="10" class="form-control"></textarea><br><button id="ask" class="btn btn-warning">Ask</button>';
+        document.querySelector("#ask").addEventListener("click",()=>{
+            const userId = JSON.parse(user).id;
+            const title = document.querySelector("#title").value;
+            const description = document.querySelector("#description").value;
+            const id = LastChar;
+            const data = {
+                user_id:userId,
+                title:title,
+                description:description,
+                id:id,
             }
-            fetch("/Qbonanza.com/php/partials/_getusername.php",dat)
-            .then(response=>response.text())
-            .then(nam =>  {
-                name = nam;
-                str+="<div class='question'><h4>"+question.title+"</h4><p>"+question.description+"</p><br><p>Posted by "+name+" on "+question.date+"</p></div>";
-                document.querySelector("#questions").innerHTML = str;
-                // console.log("data success")
-            })
-            // .catch(err => console.error(err))
+            const params = {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }
+            fetch('/Qbonanza.com/php/post.php',params)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .then
             
         })
-    })
+    }
+    
     // .catch(err => console.log(err))
 
 ;}
