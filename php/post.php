027b9d6a0a31d02afad02f94f21a5c2b76aea7bb
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2021-12-26 21:45:55 +0530

    Logged in user can now ask questions just css left

---
+++ php/post.php
@@ -0,0 +1,44 @@
+<?php
+include 'partials/_dbconnect.php';
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+    $userid = $data->user_id;  
+    $topicid = $data->topic_id; 
+    $title = $data->title;
+    $description = $data->description; 
+    $sql = "INSERT INTO `qbonanza`.`questions` (`id`, `user_id`, `topic_id`, `title`, `description`, `date`) VALUES ('', '$userid', '$topicid', '$title', '$description', current_timestamp())";
+    try{
+        //Get DB Object
+        $database = new Database();
+        //Connect
+        $db = $database->getConnection();
+        //prepare sql1 statement
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $id = $db->lastInsertId();
+        if($id){
+            $message = 'Your question was posted successfully. Now you can login to continue access to the website';
+            echo json_encode([
+                'status' => true,
+                'msg' => $message,
+            ]);
+        }else{
+            $message = 'Something went wrong. We are sorry for the inconvenience caused';
+            echo json_encode([
+                'status' => false,
+                'msg' => $message,
+            ]);
+        }
+    }catch(PDOException $e){
+        echo '"error": {"text": '.$e->getMessage().'}';
+    }
+}
+else{
+    echo json_encode([
+        'status' => false,
+        'msg' => 'NO POSTTTTT',
+    ]);
+}
+
+?>
