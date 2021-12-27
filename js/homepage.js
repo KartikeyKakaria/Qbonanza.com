990e32427d63a31934217eec026934d0d080b19c
KartikeyKakaria <kartikeykakaria2@gmail.com>
2021-12-27 12:41:36 +0530

    a github error occured, fixed it

--- js/homepage.js
+++ js/homepage.js
@@ -9,32 +9,28 @@ if (user !== null) {
 
 function displayData(data) {
     let htmlStr = "";
-    data.forEach((element)=>{
-        htmlStr+=`<div class="topic"><img src="images/topic/${element.id}.png" height="75px" width="75px" alt="USER"><button class="topicBtn" id="${element.id}">${element.name}</button></div>`;
+    data.forEach((element) => {
+        htmlStr += `<div class="topic"><img src="images/topic/${element.id}.png" height="75px" width="75px" alt="USER"><button class="topicBtn" id="${element.id}">${element.name}</button></div>`;
     })
     document.querySelector('#topics').innerHTML = htmlStr;
     let buttons = document.getElementsByClassName('topicBtn');
-    Array.from(buttons).forEach((element)=>{
-        element.addEventListener('click',(e)=>{
-            window.location = "topic.html?id="+element.id;
+    Array.from(buttons).forEach((element) => {
+            element.addEventListener('click', (e) => {
+                window.location = "topic.html?id=" + element.id;
+            })
         })
-    })
-    // console.log(Array.from(buttons));
-    // console.log(htmlStr);
+        // console.log(Array.from(buttons));
+        // console.log(htmlStr);
 }
-data = JSON.stringify({lol:"lol"});
+data = JSON.stringify({ lol: "lol" });
 let params = {
-    method:"post",
-    headers:{
+    method: "post",
+    headers: {
         "Content-Type": "application/json"
     },
-    body:data,
+    body: data,
 }
 fetch("/Qbonanza.com/php/index.php", params)
-.then(response => response.json())
-.then(data => displayData(data.topic))
-.catch(error => console.log(error))
-
-
-
-   
+    .then(response => response.json())
+    .then(data => displayData(data.topic))
+    .catch(error => console.log(error))
