a5f168fc9f5ccea461543675f36d74f07bf7e7e4
KartikeyKakaria <kartikeykakaria2@gmail.com>
2022-03-29 13:17:29 +0530

    started with search function and did the work for main file

--- js/search.js
+++ js/search.js
@@ -1,3 +1,11 @@
-window.onload = function() {
+const searchBtn = document.getElementById('searchbtn1');
+const searchBtn2 = document.getElementById('searchbtn2');
+const buttons = [searchBtn, searchBtn2];
+buttons.forEach((element) => {
+    element.addEventListener('click', e => {
 
-}
+        const query = document.querySelector("#search" + element.id[9]).value;
+        window.location = "/Qbonanza.com/search.html?query=" + query;
+
+    })
+})
